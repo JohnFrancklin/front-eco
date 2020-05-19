7c7f1e337b3e369a3ef3accce8432fb91e12730d
Hazavana <hazavana.et@gmail.com>
2020-05-20 00:20:32 +0300

    Design Authentification

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  displayPopup = 'none';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showPopup() {
+    this.displayPopup = 'block';
+  }
+
+  closeState() {
+    this.displayPopup = 'none';
   }
 
 }
