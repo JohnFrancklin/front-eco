d2e1ba28afc9ae302bee07407fde29bd247428d3
John <johnfrancklin1@gmail.com>
2020-09-15 17:17:26 +0300

    dialog

--- src/app/components/dashbord/produits/produits.component.ts
+++ src/app/components/dashbord/produits/produits.component.ts
@@ -4,6 +4,7 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatDialogContent, MatDialogCo
 import { NgxSpinnerService } from "ngx-spinner";
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+
 import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
 
@@ -81,9 +82,7 @@ export class ProduitsComponent implements OnInit {
   title = "Smartphone G10 2e";
 
   @ViewChild('imageProduit') imageProduit: TemplateRef<any>;
-
-  @ViewChild('snackBarTemplate') snackBarTemplate: TemplateRef<any>;
-
+  @ViewChild('dialogBox') dialogBox: TemplateRef<any>;
 
 
 
@@ -141,15 +140,15 @@ export class ProduitsComponent implements OnInit {
 
     if (isValid) {
       productObject["categorie"] = "5f0ff8cee892a5408c1aae39"; // assigne clé categorie dans l"objet
-      if(confirm("Voullez-vous ajouter ce produit ?")) {
+      if (confirm("Voullez-vous ajouter ce produit ?")) {
         this.produitService.createProduct(productObject).subscribe(result => {
           // console.log("create success", result);
-          this.snackBar.open("["+result['titre']+"] a été ajouté avec success", 'ok', { duration: this.durationSnackBar, panelClass: ['blue-snackbar'] });
+          this.snackBar.open("[" + result['titre'] + "] a été ajouté avec success", 'ok', { duration: this.durationSnackBar, panelClass: ['blue-snackbar'] });
           this.produits.push(result);
         });
         console.log(productObject);
       }
-      
+
     } else {
       console.log("champ encore vide");
     }
@@ -195,11 +194,11 @@ export class ProduitsComponent implements OnInit {
       // this.produitService.updatePoduct(productObject, this.oneProduit._id).subscribe();
       if (confirm("Voullez-vous modifier " + this.oneProduit.titre + "?")) {
         this.produitService.updatePoduct(productObject, this.oneProduit._id).subscribe(result => {
-          this.snackBar.open("["+result['titre']+"] a été modifié avec success", 'ok', { duration: this.durationSnackBar, panelClass: ['blue-snackbar'] });
+          this.snackBar.open("[" + result['titre'] + "] a été modifié avec success", 'ok', { duration: this.durationSnackBar, panelClass: ['blue-snackbar'] });
         });
       } else {
       }
-      console.log(productObject);
+      // console.log(productObject);
     } else {
       console.log("champ encore vide");
     }
@@ -210,55 +209,49 @@ export class ProduitsComponent implements OnInit {
     let body = {
       acteur: "rakoto"
     };
-    if (this.oneProduit.etat == 'sandbox') {      
-      if(confirm("Voullez-vous lancer le produit "+this.oneProduit.titre+ "?")) {
-        this.oneProduit.etat = "live";
-        this.produitService.launchProduct(this.oneProduit._id, body).subscribe(result => {
-          console.log("success", result);
-          /**--------------snackbar-------- blue-snackbar dans style.css----- */
-          this.snackBar.open("["+result['titre']+"]  a été lancé avec success", 'ok', { duration: this.durationSnackBar, panelClass: ['blue-snackbar'] });
-        });
-      }      
-    } else if (this.oneProduit.etat == 'live') {      
-      if(confirm("Voullez-vous archiver le produit "+this.oneProduit.titre+ "?")) {
-        this.oneProduit.etat = "archived";
-        this.produitService.archivedProduct(this.oneProduit._id, body).subscribe(result => {
-          console.log("success", result); 
-          /**--------------snackbar-------- blue-snackbar dans style.css----- */
-          this.snackBar.open("["+result['titre']+"] a été archivé avec success", 'ok', { duration: this.durationSnackBar, panelClass: ['blue-snackbar'] });
-        });
-      }
-    } else {
-      if (confirm("Etes-vous sur de voulloir supprimer " + this.oneProduit.titre + "?")) {
-        this.produitService.deleteProduct(this.oneProduit._id).subscribe(result => {
-          console.log("success", result);
-           
-          this.produits.splice(this.oneProduit, 1);    
-               
-        });
-        this.snackBar.open("["+this.oneProduit.titre+"] a été supprimé avec success", 'ok', { duration: this.durationSnackBar, panelClass: ['blue-snackbar'] });
-        this.createProduit();
-      } else {
-      }
-    
-    }
+    const dialogRef = this.dialog.open(this.dialogBox); //ouverture dialog
+    dialogRef.afterClosed().subscribe(result => {       //recuperation decision utilisateur:  result= boolean
+      if (result) {
+        if (this.oneProduit.etat == 'sandbox') {
+          this.oneProduit.etat = "live";
+          this.produitService.launchProduct(this.oneProduit._id, body).subscribe(result => {
+            console.log("success", result);
+            /**--------------snackbar-------- blue-snackbar dans style.css----- */
+            this.snackBar.open("[" + result['titre'] + "]  a été lancé avec success", 'ok', { duration: this.durationSnackBar, panelClass: ['blue-snackbar'] });
+          });
 
+        } else if (this.oneProduit.etat == 'live') {
+          this.oneProduit.etat = "archived";
+          this.produitService.archivedProduct(this.oneProduit._id, body).subscribe(result => {
+            console.log("success", result);
+            /**--------------snackbar-------- blue-snackbar dans style.css----- */
+            this.snackBar.open("[" + result['titre'] + "] a été archivé avec success", 'ok', { duration: this.durationSnackBar, panelClass: ['blue-snackbar'] });
+          });
+        } else {
+          this.produitService.deleteProduct(this.oneProduit._id).subscribe(result => {
+            console.log("success", result);
+            this.produits.splice(this.oneProduit, 1);
+          });
+          /**--------------snackbar-------- blue-snackbar dans style.css----- */
+          this.snackBar.open("[" + this.oneProduit.titre + "] a été supprimé avec success", 'ok', { duration: this.durationSnackBar, panelClass: ['blue-snackbar'] });
+          this.createProduit();
+        }
+      }
+    });
   }
 
   deleteProduct() {
-    if (confirm("Etes-vous sur de voulloir supprimer " + this.oneProduit.titre + "?")) {
-      this.produitService.deleteProduct(this.oneProduit._id).subscribe(
-        result => {
-          console.log("success", result);
+    const dialogRef = this.dialog.open(this.dialogBox); //ouverture dialog
+    dialogRef.afterClosed().subscribe(result => {       //recuperation decision utilisateur:  result= boolean
+      if (result) {
+        this.produitService.deleteProduct(this.oneProduit._id).subscribe(result => {
           this.produits.splice(this.oneProduit, 1);
         });
-        this.snackBar.open("["+this.oneProduit.titre+"] a été supprimé avec success", 'ok', { duration: this.durationSnackBar, panelClass: ['blue-snackbar'] });
-      this.createProduit();
-    } else {
-
-    }
-
-    console.log(this.oneProduit._id);
+        /**--------------snackbar-------- blue-snackbar dans style.css----- */
+        this.snackBar.open("[" + this.oneProduit.titre + "] a été supprimé avec success", 'ok', { duration: this.durationSnackBar, panelClass: ['blue-snackbar'] });
+        this.createProduit();
+      }
+    });
   }
 
 
@@ -538,11 +531,6 @@ export class ProduitsComponent implements OnInit {
       console.log(error);
     });
   }
-
-
-
-
-
 
 
 
